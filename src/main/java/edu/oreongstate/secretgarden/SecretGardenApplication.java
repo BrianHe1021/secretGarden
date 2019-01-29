e240808b4c9dc5767fb81e41a8d22f3e589e1259
lirance <lirance@users.noreply.github.com>
2019-01-29 01:41:38 -0800

    initial

---
+++ src/main/java/edu/oreongstate/secretgarden/SecretGardenApplication.java
@@ -0,0 +1,16 @@
+package edu.oreongstate.secretgarden;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("edu.oregonstate.secretgarden.mapper")
+public class SecretGardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretGardenApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/edu/oreongstate/secretgarden/service/BaseService.java
@@ -0,0 +1,12 @@
+package edu.oreongstate.secretgarden.service;
+
+public interface BaseService<Object, ObjectKey> {
+
+    int insert(Object record);
+
+    Object selectByPrimaryKey(ObjectKey objectKey);
+
+    int updateByPrimaryKey(Object object);
+
+    int deleteByPrimaryKey(ObjectKey objectKey);
+}
